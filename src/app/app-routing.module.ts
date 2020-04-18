97f9b51e4c88dc0e69603be3d0d0891ad1d03aef
zhangshifu <tiejiayrz@outlook.com>
2020-04-18 13:28:55 +0800

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
-  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
-  ];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,15 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider';
-import {MyTelComponent} from './my-tel/my-tel.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
-import { CustomStepperComponent } from './custom-stepper/custom-stepper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyTelComponent,
-    DynamicFormComponent,
-    DynamicFormQuestionComponent,
-    CustomStepperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    MatSliderModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
