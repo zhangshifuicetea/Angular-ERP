31c30f4a37680a182edafecf040e6c2ae8eaa5d0
zhangshifu <tiejiayrz@outlook.com>
2020-04-29 22:44:38 +0800

    observable

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {SharedModule} from '../../shared/shared.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { TestChildComponent } from './test-child/test-child.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    TestChildComponent,
   ],
     imports: [
         CommonModule,
---
+++ src/assets/rxjs/note.md
@@ -0,0 +1,14 @@
+# Iterators & Observers
+arrays and events are both are collections
+
+iterable and observable 
+
+iterators and observers have the same pattern (producers and consumers).
+
+pull and push
+
+Observable === Collection + Time
+
+# Obeervable
+It can model events, async server requests and animations
+
