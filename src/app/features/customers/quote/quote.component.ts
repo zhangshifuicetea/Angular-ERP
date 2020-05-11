a3c999ec371fc196e98c4f571d54ce7b14751c9a
zhangshifu <tiejiayrz@outlook.com>
2020-05-11 10:47:14 +0800

    客户 项目 报价

--- src/app/features/customers/quote/quote.component.ts
+++ src/app/features/customers/quote/quote.component.ts
@@ -7,26 +7,46 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
   styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
-  partials: Partial[] = partials;
+  partials: ProductPartial[] = partials.map(p =>
+    (new ProductPartial(p.name, p.index, p.price, p.width, p.height, p.count, p.factor, p.remark, p.usage)));
   displayedColumns = ['index', 'name', 'usage', 'width', 'height', 'count', 'factor', 'price', 'totalPrice', 'remark', 'action'];
   usageOptions = ['机体', '动力总成', '外机装甲'];
+
+  get totalPrice() {
+    return this.partials.reduce((acc, curr) => {
+      return acc + curr.totalPrice;
+    }, 0);
+  }
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.partials.forEach((p) => {
+      p.calTotalPrice();
+    });
   }
 
-  drop(event: CdkDragDrop<Partial[]>) {
+  drop(event: CdkDragDrop<ProductPartial[]>) {
     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
   }
 
   addPartial(name: string, price: string) {
+    if (!name || !price) {
+      return;
+    }
     const p: number = +price;
+    const len = (this.partials || []).length + 1;
+    const newPartial = new ProductPartial(name, len, p);
+    newPartial.calTotalPrice();
+    this.partials = this.partials.concat(newPartial);
+  }
+
+  removePartial(id: number) {
+    this.partials = this.partials.filter(p => p.id !== id);
   }
 
 }
-
 
 
 interface Partial {
@@ -45,23 +65,23 @@ interface Partial {
 
 export class ProductPartial implements Partial {
   id: number = +Date.now();
-  index: number;
-  name: string;
-  count: number;
-  factor: number;
-  height: number;
-  price: number;
-  totalPrice: number;
-  width: number;
-  remark?: string;
-  usage?: string;
+  totalPrice = 0;
 
-  constructor() {
-
+  constructor(
+    public name: string,
+    public index: number,
+    public price = 0,
+    public width = 0,
+    public height = 0,
+    public count = 1,
+    public factor = 1,
+    public remark?: string,
+    public usage?: string) {
   }
 
   calTotalPrice() {
-
+    const totalPrice = this.width * this.height * this.count * this.factor * this.price * 100 * 100 / 1000 / 1000 / 100 / 100;
+    this.totalPrice = Math.floor(totalPrice * 100) / 100;
   }
 
 }
@@ -85,7 +105,7 @@ const partials: Partial[] = [
     index: 2,
     name: '部件2',
     width: 3200,
-    height: 6000,
+    height: 1231,
     count: 1,
     factor: 1.1,
     price: 233,
@@ -94,14 +114,14 @@ const partials: Partial[] = [
     usage: '机体'
   },
   {
-    id: 222,
-    index: 2,
+    id: 333,
+    index: 3,
     name: '部件3',
     width: 1600,
     height: 2300,
     count: 3,
-    factor: 1.6,
-    price: 67,
+    factor: 12,
+    price: 1367,
     totalPrice: 0,
     remark: '',
     usage: '外机装甲'
