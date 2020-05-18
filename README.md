3a74baaba7eb75b0c9f1cbcb6ca6c787d2d265a6
zhangshifu <tiejiayrz@outlook.com>
2020-05-18 12:12:13 +0800

    README.md

--- README.md
+++ README.md
@@ -2,26 +2,16 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
+Based on [Angular Material](https://material.angular.io/).
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## Features
+1. 客户管理
+2. 产品管理：产品维护 产品编辑
+3. 项目管理：项目看板
+4. 项目报价：前端实时计算
+5. 数据统计
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/assets/algorithm.js
+++ src/assets/algorithm.js
@@ -1,18 +1,67 @@
-const mergeLinkList = (l1, l2) => {
-  const head = new NodeList();
-  let curr = head;
-
-  while (l1 && l2) {
-    if (l1.val < l2.val) {
-      curr.next = l1;
-      l1 = l1.next;
-    } else {
-      curr.next = l2;
-      l2 = l2.next;
-    }
-    curr = curr.next;
-  }
-  curr.next = l1 !== null ? l1 : l2;
-
-  return head.next;
+// 数组扁平化并去除重复数据
+const arr = [[1, 12, 9], 4, 5, [11, [7, 8], [3, [10, 7], 2]]];
+const flat = (array) => {
+  return array.reduce((acc, curr) => {
+    return Array.isArray(curr) ? acc.concat(flat(curr)) : acc.concat(curr);
+  }, [])
 }
+
+const res = [...(new Set(flat(arr)))].sort((a, b) => a - b);
+
+console.log(res);
+
+// 实现一个new
+function _new(fn, ...args) {
+  const obj = Object.create(fn.prototype);
+  const res = fn.apply(obj, args);
+  return res instanceof Object ? res : obj;
+}
+
+// sleep
+function sleep(time, callback) {
+  setTimeout(callback, time);
+}
+
+function sleepPromise(time) {
+  return new Promise(((resolve, reject) => setTimeout(resolve, time)))
+}
+
+// debounce
+function debounce(fn, time) {
+  let timer;
+  const context = this;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, time)
+  }
+}
+
+const objA = {
+  a: 1,
+  debounce,
+}
+
+const funccccc = objA.debounce;
+
+function sayHi(where) {
+  console.log(this.a, where);
+}
+
+objA.debounce(sayHi, 100)('obja');
+funccccc(sayHi, 200)('www');
+
+// throttle
+function throttle(fn, time) {
+  let activeTime = 0;
+  const context = this;
+  return (...args) => {
+    const currentTime = Date.now();
+    if (currentTime - activeTime > time) {
+      fn.apply(context, args);
+      activeTime = currentTime;
+    }
+  }
+}
+
